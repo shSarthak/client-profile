bfb7fdd9ccf461ffa17e8ef5c0c179bd4eae80e4
shSarthak <sharmasarthak648@gmail.com>
2025-07-17 22:38:09 +0530

    new new new

--- js/main.js
+++ js/main.js
@@ -352,7 +352,7 @@ function handleContactForm() {
 
         // Send email using EmailJS
         // Replace these with your actual EmailJS credentials
-        const serviceID = 'service_cev6if5'; // Replace with your EmailJS service ID
+        const serviceID = 'service_wp6oixl'; // Replace with your EmailJS service ID
         const templateID = 'template_ygeakuk'; // Replace with your EmailJS template ID
         const publicKey = 'PrXglDKKoVCdWEOyA'; // Replace with your EmailJS public key
 
